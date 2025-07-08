66031c3140410084dd3fd1c0f96bf06d6eeb089b
준영킴 <zvpdrnls@gmail.com>
2025-07-09 01:33:28 +0900

    refactor: NutrientPercentage 불변 객체 설계 및 @Setter 제거

--- BackEnd/goorm/src/main/java/backend/goorm/diet/dto/NutrientPercentage.java
+++ BackEnd/goorm/src/main/java/backend/goorm/diet/dto/NutrientPercentage.java
@@ -1,18 +1,17 @@
 package backend.goorm.diet.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class NutrientPercentage {
     private double carbsPercentage;
     private double proteinPercentage;
     private double fatPercentage;
-    private double totalCalories;   // 추가
+    private double totalCalories;
 }
-
